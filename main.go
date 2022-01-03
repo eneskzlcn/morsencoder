dc7a74db69b7923e874409a518eeae60865e2cc1
eneskzlcn <nazifenes.kizilcin@gmail.com>
2022-01-03 16:16:54 +0300

    Api created and the handler for encode operation prepared.
    Api is in working state now and implemented on main.go. Api has
    one handler of get method and route to /encode. If a query param with key
    text given to the api, then it will response with morse code encoded
    version of the given query 'text'

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "MorseCodeEncoder/encoder"
+import (
+	"MorseCodeEncoder/encoder"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
 func main(){
-	encoder.EncodeToMorseCode("","")
+	app:= fiber.New()
+	app.Get("/encode", func(c *fiber.Ctx) error {
+		var sentTextToEncode string
+		sentTextToEncode = c.Query("text")
+		log.Printf("Sended text to encode: %s",sentTextToEncode)
+		if sentTextToEncode != ""{
+			return c.SendString(encoder.EncodeToMorseCode(sentTextToEncode," "))
+		}
+		return c.SendStatus(404)
+	})
+	app.Listen(":4006")
 }
