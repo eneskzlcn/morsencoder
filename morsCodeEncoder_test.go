ed161edecb091c54a30759da1188ab8b81bcbf46
eneskzlcn <nazifenes.kizilcin@gmail.com>
2022-01-03 15:18:29 +0300

    TestISentGivenStringInMorseEncodedFormatSuccessfully test is created and tested.
    In the test, I sent a test string 'AB' to the created api which has a handler
    for a get request with query params. This handler takes this query params and
    returns its morse encoded format in response. I tested the test string's encoded
    format exactly same with the returned encoded string in response of test request's
    body. Test is passed.

--- morsCodeEncoder_test.go
+++ morsCodeEncoder_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -31,13 +32,7 @@ func TestIGetAStringAsParameterSuccessfullyFromRequest(t *testing.T){
 
 	var sentTextToEncode string
 	app.Post("/",func(c *fiber.Ctx) error{
-
 		sentTextToEncode = c.Query("text")
-		log.Printf("Sended text to encode: %s",sentTextToEncode)
-		if sentTextToEncode != ""{
-			return c.SendString(encoder.EncodeToMorseCode(sentTextToEncode," "))
-		}
-
 		return c.SendStatus(200)
 	})
 	req := httptest.NewRequest(http.MethodPost,fmt.Sprintf("/?text=%s",testText), nil)
@@ -45,4 +40,26 @@ func TestIGetAStringAsParameterSuccessfullyFromRequest(t *testing.T){
 	app.Test(req,1)
 	assert.Equalf(t,testText,sentTextToEncode,"The string sent to the api is not taking correctly." )
 }
+func TestISentGivenStringInMorseEncodedFormatSuccessfully(t *testing.T){
+	testText := "AB"
+	app := fiber.New()
 
+	app.Get("/",func(c *fiber.Ctx) error{
+		var sentTextToEncode string
+		sentTextToEncode = c.Query("text")
+		log.Printf("Sended text to encode: %s",sentTextToEncode)
+		if sentTextToEncode != ""{
+			return c.SendString(encoder.EncodeToMorseCode(sentTextToEncode," "))
+		}
+		return c.SendStatus(200)
+	})
+
+	req := httptest.NewRequest(http.MethodGet,fmt.Sprintf("/?text=%s",testText), nil)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp,_ := app.Test(req,1)
+
+	responseEncodedTextAsByte,_ := ioutil.ReadAll(resp.Body)
+	responseEncodedText := string(responseEncodedTextAsByte)
+
+	assert.Equalf(t,responseEncodedText,encoder.EncodeToMorseCode(testText," "),"The api not correctly sent the given string in morse code encoded format" )
+}
