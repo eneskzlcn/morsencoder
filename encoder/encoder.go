8a3484a0bb1f4d56564c54042a725920ead3de99
eneskzlcn <nazifenes.kizilcin@gmail.com>
2022-01-03 13:15:16 +0300

    TestIEncodeGivenStringMorseCodeSuccessfully test is almost finished
    the required encoding operation for morse alphabet done in encoder/encoder.go
    2 function created for encoding. One is EncodeToMorseCode which is public, and
    one of encodeWord which is private. EncodeToMorseCode calls encodeWord inside
    repetadly to encode the words inside given text seperately. Then all the encoded
    get merged in EncodeToMorseCode function and returning from the function.

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -1,7 +1,6 @@
 package encoder
 
 import (
-	"log"
 	"strings"
 )
 
@@ -53,7 +52,25 @@ var morseCodesMap = map[string]string{
 	")": "-.--.-",
 }
 
-func EncodeToMorseCode(text string){
-	seperatedText := strings.Split(text,"")
-	log.Println(seperatedText)
+func EncodeToMorseCode(text string,letterSplitter string) string{
+	var encodedText string
+	text = strings.ToUpper(text)
+	seperatedText := strings.Split(text, " ")
+	for _,word := range seperatedText{
+		encodedWord := encodeWord(word,letterSplitter)
+		if encodedWord != ""{
+			encodedText+= encodedWord
+		}
+	}
+	return encodedText
+}
+func encodeWord(word string,letterSplitter string) string{
+	var encodedWord string
+	for i:= 0 ; i < len(word); i++{
+		morseCode:= morseCodesMap[word[i:i+1]]
+		if morseCode != ""{
+			encodedWord+= morseCode + letterSplitter
+		}
+	}
+	return encodedWord
 }
--- morsCodeEncoder_test.go
+++ morsCodeEncoder_test.go
@@ -1,6 +1,25 @@
-package MorseCodeEncoder
+package main
 
+import (
+	"MorseCodeEncoder/encoder"
+	"log"
+	"testing"
+)
+
+/* TEST IS PROGRAM ABLE TO ENCODE STRING TO MORSE CODE FORMAT
+	* we need a library for morse alphabet to encode
+	* we need a function for encode operation.
+*/
 
 // TEST IS PROGRAM ABLE TO TAKE A STRING AS PARAMETER
-// TEST IS PROGRAM ABLE TO ENCODE STRING TO MORSE CODE FORMAT
 // TEST IS PROGRAM ABLE TO SENT BACK ENCODED MORSE CODE
+
+
+func TestIEncodeGivenStringToMorseCodeSuccessfully(t *testing.T){
+	testString := "Convert Me To Morse Code"
+	encodedString := encoder.EncodeToMorseCode(testString," ")
+	log.Println(encodedString)
+}
+//func TestIGetAStringAsParameterSuccessfullyFromRequest(t *testing.T){
+//
+//}
