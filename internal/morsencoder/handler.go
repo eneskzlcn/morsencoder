d49c4cb39f08f4b25f37cf4273905c3db769a2c6
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-09-28 09:46:36 +0300

    handler layer is done for morsencoder

---
+++ internal/morsencoder/handler.go
@@ -0,0 +1,36 @@
+package morsencoder
+
+import "github.com/gofiber/fiber/v2"
+
+type MorseEncoderService interface {
+	Encode(textToEncode string) (string, error)
+}
+type Logger interface {
+	Debugf(template string, args ...interface{})
+}
+type Handler struct {
+	service MorseEncoderService
+	logger  Logger
+}
+
+func NewHandler(service MorseEncoderService, logger Logger) *Handler {
+	if logger == nil || service == nil {
+		return nil
+	}
+	return &Handler{
+		service: service,
+		logger:  logger,
+	}
+}
+func (h *Handler) Encode(ctx *fiber.Ctx) error {
+	textToEncode := ctx.Query("text", "")
+	h.logger.Debugf("Encoding request arrived for the string %s", textToEncode)
+	encodedText, err := h.service.Encode(textToEncode)
+	if err != nil {
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+	return ctx.Status(fiber.StatusOK).SendString(encodedText)
+}
+func (h *Handler) RegisterRoutes(app *fiber.App) {
+	app.Get("/encode", h.Encode)
+}
